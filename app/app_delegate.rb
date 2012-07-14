0a890e2d6d5c7af526f7fa6dd6ee23202e6f7868
Amit Kumar <toamitkumar@gmail.com>
2012-07-14 13:10:33 +0530

    added highcharts JS library

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    # UIApplication.sharedApplication.setStatusBarHidden(true, withAnimation:UIStatusBarAnimationFade)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = WebViewController.alloc.init
+    @window.makeKeyAndVisible    
+
     true
   end
 end
